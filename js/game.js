fa4f9f432389faba975c116a1c0e0136762f11ce
OrionStar25 <chunnushrivastava@gmail.com>
2017-08-02 21:15:22 +0530

    minor changes

--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
 //function to get option from user
 function getOptionUser() {
-	
+	$('img').click(function(){
+		$('img').toggleClass();
+	})
 }
 
 //function to generate option from computer
@@ -11,57 +13,62 @@ function getOptionComputer() {
 
 //function to change the counter after every round
 function changeCounter(winner) {
-
+	if (winner === "computer") {
+		$(.'1').fadeIn(fast);//change this
+	}
 }
 
 //function to compare the options and return who won in each round
 function compare(user, computer) {
+	$user="user";
+	$computer="computer";
 	switch (user) {
 		case "rock": if(computer === "paper")
 					 {
-						changeCounter(computer);
-						return "computer";
+						changeCounter($computer);
+						return $computer;
 					 }
 					 else if (computer === "scissor") {
-						changeCounter(user);
-						return "user";
+						changeCounter($user);
+						return $user;
 					 }
 					 break;
 
 		case "paper": if (computer === "rock") {
-						changeCounter(user);
-						return "user";
+						changeCounter($user);
+						return $user;
 					 }
 					 else if (computer === "scissor") {
-					 	changeCounter(computer);
-					 	return "computer";
+					 	changeCounter($computer);
+					 	return $computer;
 					 }
 					 break;
 
 		case "scissor": if (computer === "rock") {
-							changeCounter(computer);
-							return "computer";
+							changeCounter($computer);
+							return $computer;
 						}
 						else if (computer === "paper") {
-							changeCounter(user);
-							return "user";
+							changeCounter($user);
+							return $user;
 						}
 						break;
 	}
 }
 
 //function to disply overall winner
-function display(winner) {
-	$("#winner").text("The winner is " + winner ".");
+function display($winner) {
+	$("#winner").text("The winner is " + $winner + ".");
 }
 
 $(document).ready(function() {
 	var player1 = 0; //user
 	var player2 = 0; //computer
+	$('.score').hide();
 
 	while (player2 !== 3 && player1 !== 3) {
-		var user=getOptionUser();
 		var computer=getOptionComputer();
+		var user=getOptionUser();
 		var winner=compare(user,computer);
 
 		if (winner === "user") {
@@ -71,6 +78,7 @@ $(document).ready(function() {
 			player2 = player2 + 1;
 		}		
 	}
+	//final winner
 	if (player1 === 3) {
 		display(user);
 	}
