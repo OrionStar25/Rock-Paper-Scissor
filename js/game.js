fca0f25693dcd80be0a93ec35611b74a5b5c5904
OrionStar25 <chunnushrivastava@gmail.com>
2017-08-02 18:38:36 +0530

    added some buttons

--- js/game.js
+++ js/game.js
@@ -9,24 +9,72 @@ function getOptionComputer() {
 	return options[Math.floor(Math.random()*3)];
 }
 
-//function to compare the options and return who won in each round
-function compare() {
-	
-}
-
 //function to change the counter after every round
 function changeCounter(winner) {
 
 }
 
-//function to disply overall winner
-function display() {
+//function to compare the options and return who won in each round
+function compare(user, computer) {
+	switch (user) {
+		case "rock": if(computer === "paper")
+					 {
+						changeCounter(computer);
+						return "computer";
+					 }
+					 else if (computer === "scissor") {
+						changeCounter(user);
+						return "user";
+					 }
+					 break;
 
+		case "paper": if (computer === "rock") {
+						changeCounter(user);
+						return "user";
+					 }
+					 else if (computer === "scissor") {
+					 	changeCounter(computer);
+					 	return "computer";
+					 }
+					 break;
+
+		case "scissor": if (computer === "rock") {
+							changeCounter(computer);
+							return "computer";
+						}
+						else if (computer === "paper") {
+							changeCounter(user);
+							return "user";
+						}
+						break;
+	}
+}
+
+//function to disply overall winner
+function display(winner) {
+	$("#winner").text("The winner is " + winner ".");
 }
 
 $(document).ready(function() {
-	var user=getOptionUser();
-	var computer=getOptionComputer();
-	var winner=compare();
-	changeCounter(winner);
+	var player1 = 0; //user
+	var player2 = 0; //computer
+
+	while (player2 !== 3 && player1 !== 3) {
+		var user=getOptionUser();
+		var computer=getOptionComputer();
+		var winner=compare(user,computer);
+
+		if (winner === "user") {
+			player1 = player1 + 1;
+		}
+		else {
+			player2 = player2 + 1;
+		}		
+	}
+	if (player1 === 3) {
+		display(user);
+	}
+	else {
+		display(computer);
+	}
 });
