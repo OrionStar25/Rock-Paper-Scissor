80709d0243e22cb8224561e6e8f2ba7adb228172
OrionStar25 <chunnushrivastava@gmail.com>
2017-08-02 14:44:27 +0530

    added functions

--- js/game.js
+++ js/game.js
@@ -1,9 +1,32 @@
-function getOption() {
-	$(.options).onclick();
+//function to get option from user
+function getOptionUser() {
+	
 }
-$(document).ready(function(){
-	$user=getOption();
-	function() {
 
-	}
+//function to generate option from computer
+function getOptionComputer() {
+	var options = ["rock","paper","scissor"];
+	return options[Math.floor(Math.random()*3)];
+}
+
+//function to compare the options and return who won in each round
+function compare() {
+	
+}
+
+//function to change the counter after every round
+function changeCounter(winner) {
+
+}
+
+//function to disply overall winner
+function display() {
+
+}
+
+$(document).ready(function() {
+	var user=getOptionUser();
+	var computer=getOptionComputer();
+	var winner=compare();
+	changeCounter(winner);
 });
